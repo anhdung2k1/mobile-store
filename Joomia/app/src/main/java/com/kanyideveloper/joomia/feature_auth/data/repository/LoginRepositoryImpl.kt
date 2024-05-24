f0bc1767b0551cc05394cc139ea9a78353e2b904
anhdung2k1 <19521167@gm.uit.edu.vn>
2024-05-24 08:21:59 +0700

    Add Mobile Application Kotlin

---
+++ Joomia/app/src/main/java/com/kanyideveloper/joomia/feature_auth/data/repository/LoginRepositoryImpl.kt
@@ -0,0 +1,67 @@
+package com.kanyideveloper.joomia.feature_auth.data.repository
+
+import com.kanyideveloper.joomia.core.util.Resource
+import com.kanyideveloper.joomia.feature_auth.data.dto.UserResponseDto
+import com.kanyideveloper.joomia.feature_auth.data.local.AuthPreferences
+import com.kanyideveloper.joomia.feature_auth.data.remote.AuthApiService
+import com.kanyideveloper.joomia.feature_auth.data.remote.request.LoginRequest
+import com.kanyideveloper.joomia.feature_auth.domain.repository.LoginRepository
+import kotlinx.coroutines.flow.first
+import retrofit2.HttpException
+import timber.log.Timber
+import java.io.IOException
+
+class LoginRepositoryImpl(
+    private val authApiService: AuthApiService,
+    private val authPreferences: AuthPreferences
+) : LoginRepository {
+    override suspend fun login(loginRequest: LoginRequest, rememberMe: Boolean): Resource<Unit> {
+        Timber.d("Login called")
+        return try {
+            val response = authApiService.loginUser(loginRequest)
+            Timber.d("Login Token: ${response.token}")
+
+            getAllUsers(loginRequest.username)?.let { authPreferences.saveUserdata(it) }
+
+            if (rememberMe) {
+                authPreferences.saveAccessToken(response.token)
+            }
+            Resource.Success(Unit)
+        } catch (e: IOException) {
+            Resource.Error(message = "Could not reach the server, please check your internet connection!")
+        } catch (e: HttpException) {
+            Resource.Error(message = "An Unknown error occurred, please try again!")
+        }
+    }
+
+    override suspend fun autoLogin(): Resource<Unit> {
+        val accessToken = authPreferences.getAccessToken.first()
+        Timber.d("Auto login access token: $accessToken")
+        return if (accessToken != "") {
+            Resource.Success(Unit)
+        } else {
+            Resource.Error("")
+        }
+    }
+
+    override suspend fun logout(): Resource<Unit> {
+        return try {
+            authPreferences.clearAccessToken()
+            val fetchedToken = authPreferences.getAccessToken.first()
+            Timber.d("token: $fetchedToken")
+
+            if (fetchedToken.isEmpty()) {
+                Resource.Success(Unit)
+            } else {
+                Resource.Error("Unknown Error")
+            }
+        } catch (e: Exception) {
+            return Resource.Error("Unknown error occurred")
+        }
+    }
+
+    private suspend fun getAllUsers(name: String): UserResponseDto? {
+        val response = authApiService.getAllUsers()
+        return response.find { it.username == name }
+    }
+}
